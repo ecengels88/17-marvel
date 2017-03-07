738a013f28365e64dc1a812bef93f5306fb86693
Evan Engels <evan.engels@gmail.com>
2017-03-07 15:12:03 -0600

    began testing

--- app/index.js
+++ app/index.js
@@ -3,6 +3,6 @@ import 'whatwg-fetch';
 import Vue from 'vue';
 import App from './components/application.vue';
 
-const app = new Vue(app);
+const app = new Vue(App);
 
 app.$mount('.app');
--- tests/index-test.js
+++ tests/index-test.js
@@ -1,5 +1,14 @@
 /* eslint-env qunit */
 
+
 test('it is working', (assert) => {
   assert.ok(true);
+});
+
+
+test('When loading series info replace the old series info', (assert) => {
+  const oldState = { seriesInfo: [{ title: 'Spider-Girl', dates: '2010-2011' }] };
+  const actionOne = { type: 'SERIES_INFO@LOAD_COMPLETE', data: [{ title: 'Red Hulk', dates: '2005-2007' }] };
+
+  assert.deepEqual(reducer(oldState, actionOne), { seriesInfo: actionOne.data });
 });

f828a9ed161728c144d6757c791480c07f936dbe
Evan Engels <evan.engels@gmail.com>
2017-03-08 16:24:08 -0600

    passed my tests

--- app/actions.js
+++ app/actions.js
@@ -3,14 +3,10 @@ export function loadSeries(data) {
 }
 
 export function findCharacters(data) {
-  data.id = new Date();
-
   return { type: 'CHARACTERS@FIND_ALL_COMPLETE', data };
 }
 
 export function findComics(id) {
-  data.id = new Date();
-
   return { type: 'COMICS@FIND_ALL_COMPLETE', data: id };
 }
 
@@ -22,9 +18,9 @@ export function clearComics(id) {
   return { type: 'MODAL@CLEAR', data: id };
 }
 
-export function seriesInfoSearch(name) {
+export function seriesInfoSearch(seriesInfo) {
   return (dispatch) => {
-    fetch('http://marvel-is-broke.herokuapp.com/series?limit=1&titleStartsWith=TITLE&apikey=APIKEY')
+    fetch(`http://marvel-is-broke.herokuapp.com/series?limit=1&titleStartsWith=TITLE&apikey=${seriesInfo}`)
    .then(r = r.json())
    .then((data) => {
      dispatch({
